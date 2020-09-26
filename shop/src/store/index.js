2585ab069d5766634fdb3dc2141a61a78dbf9d39
yangxianguang <3232@126.com>
2020-09-27 07:28:39 +0800

    vue.config.js

--- shop/src/store/index.js
+++ shop/src/store/index.js
@@ -1,3 +1,4 @@
+import { startCase } from 'lodash'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -5,11 +6,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    counter:1000
   },
   mutations: {
+    increment(state){
+      state.counter++
+    },
+
+    decrement(state){
+      state.counter--
+    }
   },
   actions: {
   },
+
+  getters:{
+    powercounter(state){
+      return state.counter*state.counter
+    }
+  },
+
   modules: {
   }
 })
--- shop/src/views/Welcome.vue
+++ shop/src/views/Welcome.vue
@@ -1,5 +1,30 @@
 <template>
     <div>
         <h1>Welcome</h1>
+        <div>
+            {{$store.state.counter}}
+        </div>
+        <div>{{$store.getters.powercounter}}</div>
+        <el-button @click="addition"></el-button>
+        <el-button @click="subtraction"></el-button>
     </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+
+    methods:{
+        addition(){
+            this.$store.commit("increment")
+        },
+        subtraction(){
+            this.$store.commit("decrement")
+        },
+    }
+}
+</script>
